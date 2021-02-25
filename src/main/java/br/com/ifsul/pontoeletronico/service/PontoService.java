726f3a500f7f5f20afb57c512e1636c6bc0354a7
guilhXavier <guilhermetamara5@gmail.com>
2021-02-25 20:15:39 -0300

    finalizando services

---
+++ src/main/java/br/com/ifsul/pontoeletronico/service/PontoService.java
@@ -0,0 +1,69 @@
+package br.com.ifsul.pontoeletronico.service;
+
+import br.com.ifsul.pontoeletronico.model.Ponto;
+import br.com.ifsul.pontoeletronico.model.Tarefa;
+import br.com.ifsul.pontoeletronico.repository.PontoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PontoService {
+
+    private final PontoRepository pontoRepository;
+
+    public String cadastrarPonto(List<Tarefa> tarefas) {
+        Ponto ponto = Ponto.builder().primeiroPonto(LocalTime.now()).data(new Date(System.currentTimeMillis())).build();
+
+        pontoRepository.save(ponto);
+
+        return "Salvo!";
+    }
+
+    public String registrarPonto(Integer id) {
+        Ponto ponto = pontoRepository.findById(id).orElse(new Ponto());
+
+        pontoRepository.save(registrarProximoPonto(ponto));
+
+        return "Novo ponto registrado.";
+    }
+
+    public String adicionarTarefa(Integer id, Tarefa tarefa) {
+        Ponto ponto = pontoRepository.findById(id).orElse(new Ponto());
+        List<Tarefa> tarefaList = ponto.getTarefas();
+        tarefaList.add(tarefa);
+        ponto.setTarefas(tarefaList);
+
+        pontoRepository.save(ponto);
+
+        return "Novo ponto registrado.";
+    }
+
+    public Optional<Ponto> encontrarPontoPorId(Integer id) {
+        return pontoRepository.findById(id);
+    }
+
+    private Ponto registrarProximoPonto(Ponto ponto) {
+        if (ponto.getSegundoPonto() == null) {
+            ponto.setSegundoPonto(LocalTime.now());
+            return ponto;
+        }
+
+        if (ponto.getTerceiroPonto() == null) {
+            ponto.setTerceiroPonto(LocalTime.now());
+            return ponto;
+        }
+
+        if (ponto.getQuartoPonto() == null) {
+            ponto.setQuartoPonto(LocalTime.now());
+            return ponto;
+        }
+
+        return ponto;
+    }
+}
--- src/main/java/br/com/ifsul/pontoeletronico/service/TarefaService.java
+++ src/main/java/br/com/ifsul/pontoeletronico/service/TarefaService.java
@@ -1,6 +1,8 @@
 package br.com.ifsul.pontoeletronico.service;
 
+import br.com.ifsul.pontoeletronico.model.Ponto;
 import br.com.ifsul.pontoeletronico.model.Tarefa;
+import br.com.ifsul.pontoeletronico.model.Usuario;
 import br.com.ifsul.pontoeletronico.repository.TarefaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,32 @@ public class TarefaService {
 
     private final TarefaRepository tarefaRepository;
 
-    public String cadastrarTarefa(String nome, LocalTime localTime, List<Integer> idUsuarios, List<Integer> idPontos) {
-        return "azar";
+    public String cadastrarTarefa(String nome, LocalTime localTime, List<Usuario> usuarios, List<Ponto> pontos) {
+        tarefaRepository.save(buildTarefa(nome, localTime, usuarios, pontos));
+
+        return "Salvo!";
+    }
+
+    public String editarTarefa(Integer id, Tarefa tarefa) {
+        Tarefa tarefa1 = buildTarefa(tarefa.getNome(), tarefa.getTrabalhoCompleto(), tarefa.getUsuarios(), tarefa.getPontosTrabalhados());
+        tarefa1.setId(id);
+
+        tarefaRepository.save(tarefa1);
+
+        return "Editado!";
+    }
+
+    public String excluirTarefa(Integer id) {
+        tarefaRepository.deleteById(id);
+
+        return "Excluido!";
+    }
+
+    public List<Tarefa> buscarTarefas() {
+        return tarefaRepository.findAll();
+    }
+
+    private Tarefa buildTarefa(String nome, LocalTime localTime, List<Usuario> idUsuarios, List<Ponto> idPontos) {
+        return Tarefa.builder().nome(nome).trabalhoCompleto(localTime).usuarios(idUsuarios).pontosTrabalhados(idPontos).build();
     }
 }
