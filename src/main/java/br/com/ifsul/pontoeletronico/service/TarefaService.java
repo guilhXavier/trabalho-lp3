e6f941cd837a9762b44d945316924d04c3539334
guilhXavier <guilhermetamara5@gmail.com>
2021-02-24 22:38:46 -0300

    deu por hj

--- src/main/java/br/com/ifsul/pontoeletronico/service/TarefaService.java
+++ src/main/java/br/com/ifsul/pontoeletronico/service/TarefaService.java
@@ -5,10 +5,16 @@ import br.com.ifsul.pontoeletronico.repository.TarefaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TarefaService {
 
     private final TarefaRepository tarefaRepository;
 
+    public String cadastrarTarefa(String nome, LocalTime localTime, List<Integer> idUsuarios, List<Integer> idPontos) {
+        return "azar";
+    }
 }
